7809889afc788e2dfb71f3dbd2916b5c15e35ad5
jihoon <pkrwlgns64@gmail.com>
2024-08-18 00:33:21 +0900

    메인화면 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import {BrowserRouter,Routes,Route, Outlet} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./page/nav";
+import Main1 from "./page/main1";
+import Banner from "./page/banner";
+import Features from "./page/features";
 
 function App() {
   return (
-   < BrowserRouter>
-    < Routes>
-      < Route path="/" element={<Nav />}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route path="/" element={<Main1 />} />
+          <Route path="/" element={<Banner />} />
+          <Route path="/" element={<Features />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/page/nav.js
+++ src/page/nav.js
@@ -1,18 +1,42 @@
-import { Link,Outlet } from "react-router-dom"
-export default ()=>{
-    return(<section id="header">
+import { Link, Outlet } from "react-router-dom";
+import Footer from "./footer";
+export default () => {
+  return (
+    <>
+      <section id="header">
         <div className="container">
-                <h1 id="logo"><a href="index.html">Strongly Typed</a></h1>
-                <p>A responsive HTML5 site template. Manufactured by HTML5 UP.</p>
-                <nav id="nav">
-                    <ul>
-                        <li><Link className="icon solid fa-home" to="/"><span>Introduction</span></Link></li>
-                        <li><a className="icon solid fa-cog" href="left-sidebar.html"><span>Left Sidebar</span></a></li>
-                        <li><a className="icon solid fa-retweet" href="right-sidebar.html"><span>Right Sidebar</span></a></li>
-                        <li><a className="icon solid fa-sitemap" href="no-sidebar.html"><span>No Sidebar</span></a></li>
-                    </ul>
-                </nav>
-                <Outlet/>
+          <h1 id="logo">
+            <a href="index.html">Strongly Typed</a>
+          </h1>
+          <p>A responsive HTML5 site template. Manufactured by HTML5 UP.</p>
+          <nav id="nav">
+            <ul>
+              <li>
+                <Link className="icon solid fa-home" to="/">
+                  <span>Introduction</span>
+                </Link>
+              </li>
+              <li>
+                <a className="icon solid fa-cog" href="left-sidebar.html">
+                  <span>Left Sidebar</span>
+                </a>
+              </li>
+              <li>
+                <a className="icon solid fa-retweet" href="right-sidebar.html">
+                  <span>Right Sidebar</span>
+                </a>
+              </li>
+              <li>
+                <a className="icon solid fa-sitemap" href="no-sidebar.html">
+                  <span>No Sidebar</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <Outlet />
         </div>
-    </section>)
-}
+      </section>
+      <Footer />
+    </>
+  );
+};
